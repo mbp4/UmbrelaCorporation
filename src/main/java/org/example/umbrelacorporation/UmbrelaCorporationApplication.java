a1d8bcbd21d68ac8deda5539f42839965a2787ca
mbp4 <mblanpon@myuax.com>
2024-10-12 12:35:42 +0200

    conseguimos abrir la pagina web con un metodo que cambia dependiendo del SO del ordenador que se este usando

--- src/main/java/org/example/umbrelacorporation/UmbrelaCorporationApplication.java
+++ src/main/java/org/example/umbrelacorporation/UmbrelaCorporationApplication.java
@@ -3,11 +3,33 @@ package org.example.umbrelacorporation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class UmbrelaCorporationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UmbrelaCorporationApplication.class, args);
+        openBrowser("http://localhost:8080/");
     }
 
+    private static void openBrowser(String url) {
+        String os = System.getProperty("os.name").toLowerCase();
+        try {
+            if (os.contains("win")) {
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else if (os.contains("mac")) {
+                Runtime.getRuntime().exec("open " + url);
+            } else if (os.contains("nix") || os.contains("nux")) {
+                Runtime.getRuntime().exec("xdg-open " + url);
+            } else {
+                System.err.println("Sistema operativo no soportado para abrir el navegador.");
+            }
+        } catch (IOException e) {
+            System.err.println("Error al abrir el navegador: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
+
+

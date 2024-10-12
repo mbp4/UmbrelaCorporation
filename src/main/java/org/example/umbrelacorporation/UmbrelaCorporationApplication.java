9636731cde9e87559b72c0fd99531bbaefac3d0a
mbp4 <mblanpon@myuax.com>
2024-10-12 12:13:37 +0200

    Initial commit

---
+++ src/main/java/org/example/umbrelacorporation/UmbrelaCorporationApplication.java
@@ -0,0 +1,13 @@
+package org.example.umbrelacorporation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrelaCorporationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmbrelaCorporationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/umbrelacorporation/UmbrelaCorporationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.umbrelacorporation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrelaCorporationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

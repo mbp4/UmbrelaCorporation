6a4de5932bc8755e4fef06f8fab2d2fc50f3ad7d
sonia <stejerec@myuax.com>
2024-10-16 09:25:22 +0200

    Update ControladorInicial.java

--- src/main/java/org/example/umbrelacorporation/controllers/ControladorInicial.java
+++ src/main/java/org/example/umbrelacorporation/controllers/ControladorInicial.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class controladorInicial {
+public class ControladorInicial {
 
     @GetMapping("/")
     public String inicio(){
-        return "index";
+        return "index";  // Retorna la vista "index.html" ubicada en /templates
     }
 }
